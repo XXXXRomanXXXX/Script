cc56318fad2b64252ef45a0bd60e8ef4861d76be
XXXXRomanXXXX <tymofieievroman@gmail.com>
2025-08-17 11:41:30 +0200

    Update startComfy2025.sh

--- startComfy2025.sh
+++ startComfy2025.sh
@@ -31,10 +31,11 @@ apt update -y
 apt install -y psmisc
 
 # Убиваем процесс, если порт 3000 занят
-fuser -k 3000/tcp || true
+fuser -k 1111/tcp || true
 
 # Запуск ComfyUI
 cd /workspace/ComfyUI
 source venv/bin/activate
 python main.py --listen 0.0.0.0 --port 1111
+
 
